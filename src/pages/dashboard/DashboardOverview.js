8b6f6fe8434092260a3418acb4f4135f82f52ecd
Juan Lozano <jd.lozanom@uniandes.edu.co>
2021-12-01 23:00:15 -0500

    Axios correcion

--- src/pages/dashboard/DashboardOverview.js
+++ src/pages/dashboard/DashboardOverview.js
@@ -1,5 +1,5 @@
 
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCashRegister, faChartLine, faCloudUploadAlt, faPlus, faRocket, faTasks, faUserShield } from '@fortawesome/free-solid-svg-icons';
 import { Col, Row, Button, Dropdown, ButtonGroup } from '@themesberg/react-bootstrap';
@@ -13,16 +13,21 @@ import { trafficShares, totalOrders } from "../../data/charts";
 const URL = "https://gist.githubusercontent.com/jecanizarez/d02d54e96571c6e2f24aa49ce399cfdf/raw/16ad8b5e8ef94feb969f7c41ff81a8c9507fb7c3/Data_42Hacks.json"
 
 export default () => {
-const [data, setData] = useState({ hits: [] });
+  const [data, setData] = useState({ hits: [] });
 
-useEffect(async () => {
+  useEffect(() => {
+    getData();
+  }, []);
+
+
+  const getData = async () => {
     const result = await axios(
-        URL,
-      );
-  
-      setData(result);
-      console.log(data)
-}, []);
+      URL,
+    );
+
+    setData(result.data);
+  }
+  console.log(data);
   return (
     <>
       <div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center py-4">
@@ -49,7 +54,7 @@ useEffect(async () => {
           />
         </Col>
         <Col xs={12} sm={6} xl={6} className="mb-4">
-        <CircleChartWidget
+          <CircleChartWidget
             title="Traffic Share"
             data={trafficShares} />
         </Col>
@@ -84,7 +89,7 @@ useEffect(async () => {
                 </Col>
 
                 <Col xs={12} className="px-0 mb-4">
-                 
+
                 </Col>
 
                 <Col xs={12} className="px-0">
