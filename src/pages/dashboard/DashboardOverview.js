ed3595fa98246ac298fe19609432195ecdbdb73e
Juan Cañizarez <je.canizarez@uniandes.edu.co>
2021-12-02 03:40:19 +0000

    feat(axios): added axios module

--- src/pages/dashboard/DashboardOverview.js
+++ src/pages/dashboard/DashboardOverview.js
@@ -1,14 +1,28 @@
 
-import React from "react";
+import React, {useState, useEffect} from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCashRegister, faChartLine, faCloudUploadAlt, faPlus, faRocket, faTasks, faUserShield } from '@fortawesome/free-solid-svg-icons';
 import { Col, Row, Button, Dropdown, ButtonGroup } from '@themesberg/react-bootstrap';
+import axios from 'axios';
 
 import { CounterWidget, CircleChartWidget, BarChartWidget, TeamMembersWidget, ProgressTrackWidget, RankingWidget, SalesValueWidget, SalesValueWidgetPhone, AcquisitionWidget } from "../../components/Widgets";
 import { PageVisitsTable } from "../../components/Tables";
 import { trafficShares, totalOrders } from "../../data/charts";
 
+
+const URL = "https://gist.githubusercontent.com/jecanizarez/d02d54e96571c6e2f24aa49ce399cfdf/raw/16ad8b5e8ef94feb969f7c41ff81a8c9507fb7c3/Data_42Hacks.json"
+
 export default () => {
+const [data, setData] = useState({ hits: [] });
+
+useEffect(async () => {
+    const result = await axios(
+        URL,
+      );
+  
+      setData(result);
+      console.log(data)
+}, []);
   return (
     <>
       <div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center py-4">
@@ -34,29 +48,13 @@ export default () => {
             percentage={10.57}
           />
         </Col>
-        <Col xs={12} sm={6} xl={4} className="mb-4">
-          <CounterWidget
-            category="Customers"
-            title="345k"
-            period="Feb 1 - Apr 1"
-            percentage={18.2}
-            icon={faChartLine}
-            iconColor="shape-secondary"
-          />
+        <Col xs={12} sm={6} xl={6} className="mb-4">
+        <CircleChartWidget
+            title="Traffic Share"
+            data={trafficShares} />
         </Col>
 
-        <Col xs={12} sm={6} xl={4} className="mb-4">
-          <CounterWidget
-            category="Revenue"
-            title="$43,594"
-            period="Feb 1 - Apr 1"
-            percentage={28.4}
-            icon={faCashRegister}
-            iconColor="shape-tertiary"
-          />
-        </Col>
-
-        <Col xs={12} sm={6} xl={4} className="mb-4">
+        <Col xs={12} sm={6} xl={6} className="mb-4">
           <CircleChartWidget
             title="Traffic Share"
             data={trafficShares} />
@@ -71,7 +69,6 @@ export default () => {
                 <Col xs={12} className="mb-4">
                   <PageVisitsTable />
                 </Col>
-                    <RankingWidget />
                 <Col xs={12} lg={6} className="mb-4">
                 </Col>
 
@@ -83,11 +80,7 @@ export default () => {
             <Col xs={12} xl={4}>
               <Row>
                 <Col xs={12} className="mb-4">
-                  <BarChartWidget
-                    title="Total orders"
-                    value={452}
-                    percentage={18.2}
-                    data={totalOrders} />
+
                 </Col>
 
                 <Col xs={12} className="px-0 mb-4">
