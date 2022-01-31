23a876c18358e6e43c0b98277b73df17200228c6
Zoltán Szőgyényi <zoltan.szogyenyi@gmail.com>
2022-01-31 09:15:14 +0200

    Fix the NPM login error and update packages.

--- src/index.js
+++ src/index.js
@@ -21,7 +21,6 @@ import { HashRouter } from "react-router-dom";
 import "./scss/volt.scss";
 
 // vendor styles
-import "@fortawesome/fontawesome-free/css/all.css";
 import "react-datetime/css/react-datetime.css";
 
 import HomePage from "./pages/HomePage";
